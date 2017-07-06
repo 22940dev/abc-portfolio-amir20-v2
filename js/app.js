57ec4ae29051524c692d538de7faf0ed517da196
Amir Raminfar <findamir@gmail.com>
2017-07-05 18:37:49 -0700

    Site rebuild Wed Jul  5 18:37:40 PDT 2017

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 import "../css/app.css";
-import "typed.js/js/typed";
+import Typed from 'typed.js';
 
-const target = $('#typed');
-const typerString = target.text();
-target.text('').typed({strings: [typerString], showCursor: false, typeSpeed: 20, startDelay: 700});
+const element = document.getElementById("typed");
+const text = element.textContent;
+element.textContent = '';
+
+new Typed(element, {
+    strings: [text],
+    showCursor: false,
+    typeSpeed: 40,
+    startDelay: 700
+});
