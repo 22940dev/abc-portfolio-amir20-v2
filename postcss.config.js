685275d27a3bcfd7e66b89ead8531efeec91c405
Amir Raminfar <findamir@gmail.com>
2021-03-16 08:12:34 -0700

    Uses tailwind jit instead

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/jit": {},
     "postcss-nested": {},
     "postcss-font-magician": { display: "swap" },
     autoprefixer: {},
