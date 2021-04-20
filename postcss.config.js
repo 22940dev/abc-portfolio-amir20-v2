86d9f105ec2c7a1f7848af9b9f559220a9027c9d
Amir Raminfar <findamir@gmail.com>
2021-04-20 12:09:15 -0700

    Updates tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    "@tailwindcss/jit": {},
+    "tailwindcss": {},
     "postcss-nested": {},
     "postcss-font-magician": { display: "swap" },
     autoprefixer: {},
