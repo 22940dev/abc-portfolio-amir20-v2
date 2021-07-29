c77fb9043d86cc97b769010d00c9cb1781aec067
Alec Campbell <88142969+22940dev@users.noreply.github.com>
2021-07-29 12:53:31 -0400

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,17 +2,12 @@
   <div class="w-auto md:w-3/4 lg:w-1/2">
     <h1 class="mb-8 text-5xl">Hello there.</h1>
     <p class="text-lg font-light">
-      I am a software engineer and entrepreneur. I have started two companies in my life which has shaped me in more ways that I can imagine. I enjoy using new
-      technology and learning about new patterns. I mostly specialize in web technologies but also focus on ML, big data and real-time architecture. You can
-      find some of my projects on Github.
+      I am a software engineer and entrepreneur. 
     </p>
     <br />
 
     <p class="text-lg font-light">
-      I was born in Iran and moved to Seattle at a young age. I started programming in high school and studied computer science at the George Washington
-      University in Washington DC. After graduating, I worked at AOL on aol.com which taught me a lot about scalability. I left to join Opower in Arlington, VA.
-      Opower started my career in management. I left to start my own in Los Angeles and eventually moved to San Francisco where I joined Opower for their IPO in
-      2014. I led teams at Quid, Capital One labs in SF and data and infrastructure at UJET.
+      I was born in Iran and moved to Seattle at a young age. 
     </p>
     <br />
 
